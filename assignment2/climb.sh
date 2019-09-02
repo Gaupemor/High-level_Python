5007331ba0a5c0b54d6655528952466d6f225658
Selma F{revaag Skaare <selmafs@student.matnat.uio.no>
2019-09-02 19:40:21 +0200

    2.1 climb - added error message

--- assignment2/climb.sh
+++ assignment2/climb.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
+error_message="This command only accepts one integer argument."
+
 function climb() {
 	i=1
 
+	#on argument passed
 	if [ $# -gt 0 ]; then
-		i=$1
+		#error case - if not integer arg OR more than one arg
+		if ! [[ $1 =~ ^[0-9]+$ ]] || (($# > 2 )); then
+			echo $error_message
+			return
+		else
+			i=$1
+		fi
 	fi
-	
+
 	for num in $(seq 1 $i);
 	do cd ../; done;
 }
